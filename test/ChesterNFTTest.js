dc7a60c6f462c08bd9597a5dc7fb8fd536b91333
Nico Pizzo <npizzo392@gmail.com>
2021-12-25 13:22:24 -0500

    more tests

--- test/ChesterNFTTest.js
+++ test/ChesterNFTTest.js
@@ -122,7 +122,7 @@ contract("ChesterNFT", async accounts => {
       assert.include(err.message, "revert", "The error message should contain 'revert'");
     }
 
-    var bal = await chester.balanceOf(accounts[2]);
+    var bal = await chester.balanceOf(user);
     assert.equal(bal.words[0], 0);
   });
 
@@ -138,7 +138,41 @@ contract("ChesterNFT", async accounts => {
       assert.include(err.message, "revert", "The error message should contain 'revert'");
     }
 
-    var bal = await chester.balanceOf(accounts[2]);
+    var bal = await chester.balanceOf(user);
     assert.equal(bal.words[0], 0);
   });
+});
+
+// give tests
+contract("ChesterNFT", async accounts => {
+  it("should give 1 from owner to another user", async () => {
+    var chester = await createContract();
+    var user = accounts[5];
+
+    await chester.give(user, 1);
+
+    var bal = await chester.balanceOf(user)
+    assert.equal(bal.words[0], 1);
+  });
+
+  it("should not give, not owner", async () =>{
+    var chester = await createContract();
+    var user = accounts[2];
+
+    try{
+      await chester.give(user, 1, {from: user});
+      assert.fail("The transaction should have thrown an error.");
+    }
+    catch(err){
+      assert.include(err.message, "revert", "The error message should contain 'revert'");
+    }
+    var bal = await chester.balanceOf(user)
+    assert.equal(bal.words[0], 0);
+  });
+
+  // give bad mint count
+
+  // give max supply exceeded
+
+  //
 });
