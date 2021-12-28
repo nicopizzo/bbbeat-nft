0424ed86a1bb96ca9ef244223ff13660ef42be39
Nico Pizzo <funkiernails@yahoo.com>
2021-12-28 16:30:48 -0500

    updated configs and deployment

--- migrations/2_chester.js
+++ migrations/2_chester.js
@@ -1,13 +1,44 @@
 var ChesterNFT = artifacts.require("ChesterNFT");
 
-const maxTokens = 5;
-const mintCost = "100000000000000000";
-const maxMintPerUser = 3;
-const maxGiveSupply = 3;
-const privateMintCost = "50000000000000000";
-const privateMintPerUser = 2;
-const provHash = "2E99758548972A8E8822AD47FA1017FF72F06F3FF6A016851F45C398732BC50C";
+var testParams = {
+  maxTokens: 5,
+  mintCost: "100000000000000000",
+  maxMintPerUser: 3,
+  maxGiveSupply: 3,
+  privateMintCost: "50000000000000000",
+  privateMintPerUser: 2,
+  provHash: "2E99758548972A8E8822AD47FA1017FF72F06F3FF6A016851F45C398732BC50C"
+};
+
+var ropstenParams = {
+  maxTokens: 5,
+  mintCost: "10000000000000000",
+  maxMintPerUser: 1,
+  maxGiveSupply: 1,
+  privateMintCost: "5000000000000000",
+  privateMintPerUser: 1,
+  // TODO, calculate prov for chester collection
+  provHash: "2E99758548972A8E8822AD47FA1017FF72F06F3FF6A016851F45C398732BC50C"
+};
 
 module.exports = function(deployer) {
-  deployer.deploy(ChesterNFT, maxTokens, mintCost, maxMintPerUser, maxGiveSupply, privateMintCost, privateMintPerUser, provHash);
+  // for unit tests use parameters below
+  deployer.deploy(ChesterNFT, 
+    testParams.maxTokens, 
+    testParams.mintCost, 
+    testParams.maxMintPerUser, 
+    testParams.maxGiveSupply, 
+    testParams.privateMintCost, 
+    testParams.privateMintPerUser, 
+    testParams.provHash);
+
+  // for ropsten
+  // deployer.deploy(ChesterNFT, 
+  //   ropstenParams.maxTokens, 
+  //   ropstenParams.mintCost, 
+  //   ropstenParams.maxMintPerUser, 
+  //   ropstenParams.maxGiveSupply, 
+  //   ropstenParams.privateMintCost, 
+  //   ropstenParams.privateMintPerUser, 
+  //   ropstenParams.provHash);
 };
