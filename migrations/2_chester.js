62248e0df1c37e6a0d697587263edec820dc8c7b
Nico Pizzo <funkiernails@yahoo.com>
2021-12-29 16:28:54 -0500

    added rinkeby network

--- migrations/2_chester.js
+++ migrations/2_chester.js
@@ -11,39 +11,37 @@ var testParams = {
   provHash: "2E99758548972A8E8822AD47FA1017FF72F06F3FF6A016851F45C398732BC50C"
 };
 
-var ropstenParams = {
+var publicTestParams = {
   maxTokens: 5,
   mintCost: "10000000000000000",
   maxMintPerUser: 1,
-  maxGiveSupply: 1,
+  maxGiveSupply: 4,
   privateMintCost: "5000000000000000",
   privateMintPerUser: 1,
-  // TODO setup premint
-  preMintUri: "https://premint/",
-  // TODO, calculate prov for chester collection
-  provHash: "2E99758548972A8E8822AD47FA1017FF72F06F3FF6A016851F45C398732BC50C"
+  preMintUri: "https://gateway.pinata.cloud/ipfs/QmcfhnwDvQYxv5BSLCx7GGZqJxCT1rxiWmUCJJQK7LDDBd/",
+  provHash: "ce35cec7d507b6ac395ffbde47e8521384be4bbae368e2e1423ca44254c6cc59"
 };
 
 module.exports = function(deployer) {
   // for unit tests use parameters below
-  deployer.deploy(ChesterNFT, 
-    testParams.maxTokens, 
-    testParams.mintCost, 
-    testParams.maxMintPerUser, 
-    testParams.maxGiveSupply, 
-    testParams.privateMintCost, 
-    testParams.privateMintPerUser, 
-    testParams.preMintUri,
-    testParams.provHash);
+  // deployer.deploy(ChesterNFT, 
+  //   testParams.maxTokens, 
+  //   testParams.mintCost, 
+  //   testParams.maxMintPerUser, 
+  //   testParams.maxGiveSupply, 
+  //   testParams.privateMintCost, 
+  //   testParams.privateMintPerUser, 
+  //   testParams.preMintUri,
+  //   testParams.provHash);
 
   // for ropsten
-  // deployer.deploy(ChesterNFT, 
-  //   ropstenParams.maxTokens, 
-  //   ropstenParams.mintCost, 
-  //   ropstenParams.maxMintPerUser, 
-  //   ropstenParams.maxGiveSupply, 
-  //   ropstenParams.privateMintCost, 
-  //   ropstenParams.privateMintPerUser, 
-  //   ropstenParams.preMintUri,
-  //   ropstenParams.provHash);
+  deployer.deploy(ChesterNFT, 
+    publicTestParams.maxTokens, 
+    publicTestParams.mintCost, 
+    publicTestParams.maxMintPerUser, 
+    publicTestParams.maxGiveSupply, 
+    publicTestParams.privateMintCost, 
+    publicTestParams.privateMintPerUser, 
+    publicTestParams.preMintUri,
+    publicTestParams.provHash);
 };
