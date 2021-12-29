6f73b28a180a0d19ee6036d1589e03fb5df26be5
Nico Pizzo <funkiernails@yahoo.com>
2021-12-29 17:15:55 -0500

    more code cleanup

--- contracts/ChesterNFT.sol
+++ contracts/ChesterNFT.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
