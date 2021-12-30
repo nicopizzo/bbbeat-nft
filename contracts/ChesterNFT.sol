9d6b8298943c4e465636b76f8fedce05d8c28d61
Nico Pizzo <funkiernails@yahoo.com>
2021-12-29 20:54:23 -0500

    solidity complier update

--- contracts/ChesterNFT.sol
+++ contracts/ChesterNFT.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.11;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
 
 contract ChesterNFT is ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
