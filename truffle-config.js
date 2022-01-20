510adf1f0c486d03bf426345a8fa83d9226631ae
Nico Pizzo <funkiernails@yahoo.com>
2022-01-19 21:25:55 -0500

    enabled optimizer

--- truffle-config.js
+++ truffle-config.js
@@ -59,7 +59,7 @@ module.exports = {
     rinkeby: {
       provider: () => new HDWalletProvider(privateKeys, `https://rinkeby.infura.io/v3/${projectId}`, 0, 1),
       network_id: 4,       // rinkeby's id
-      gas: 5000000,        // rinkeby has a lower block limit than mainnet
+      gas: 2615005,        // rinkeby has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
@@ -82,13 +82,13 @@ module.exports = {
     solc: {
       version: "0.8.11",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
       //  evmVersion: "byzantium"
-      // }
+      }
     }
   },
   plugins: ['truffle-plugin-verify'],
